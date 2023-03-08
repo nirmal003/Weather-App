e240f14287dda14a2c929360826324e8f74520ab
Nirmal <nirmalraj1025@gmail.com>
2023-03-08 19:30:31 +0530

    first commit

---
+++ client/src/App.style.ts
@@ -0,0 +1,89 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+  background-color: #eee;
+  margin: 5px 30px;
+  border-radius: 10px;
+  border: 2px dashed gray;
+  padding: 6px 40px 14px;
+`;
+
+export const Header = styled.div`
+  font-size: 2rem;
+  font-family: Arial, Helvetica, sans-serif;
+  font-weight: 300;
+  text-align: center;
+`;
+
+export const From = styled.form`
+display: flex;
+justify-content: space-evenly;
+padding: 30px 0 25px;
+}
+`;
+
+export const Input = styled.input`
+  padding: 8px;
+  font-size: 1rem;
+  background-color: rgba(203, 235, 235, 0.412);
+  border: none;
+  border-bottom: 2px dashed gray;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+export const Button = styled.button`
+  color: white;
+  background-color: rgba(14, 81, 133, 0.81);
+  border-radius: 20px;
+  width: 70px;
+  font-size: 1rem;
+`;
+
+export const Report = styled.div`
+  display: flex;
+  flex-direction: column;
+  border: 2px solid black;
+  border-radius: 15px;
+  padding: 8px;
+  background-color: #e8e6e6;
+`;
+
+export const Time = styled.span`
+  font-size: 13.5px;
+  color: #848383;
+`;
+
+export const Temp = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+export const Degree = styled.div`
+  font-size: 3rem;
+  font-weight: 700;
+  padding: 8px;
+`;
+
+export const Icon = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Data = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 50px;
+  padding: 5px 10px 5px 5px;
+`;
+
+export const Grid = styled.div`
+  border-bottom: 2px dashed gray;
+  padding: 20px 0;
+  display: flex;
+  justify-content: space-between;
+`;
---
+++ client/src/App.tsx
@@ -0,0 +1,269 @@
+import axios from "axios";
+import { FormEvent, useEffect, useState } from "react";
+import {
+  Button,
+  Data,
+  Degree,
+  From,
+  Grid,
+  Header,
+  Icon,
+  Input,
+  Report,
+  Temp,
+  Time,
+  Wrapper,
+} from "./App.style";
+// import styled from "styled-components";
+
+function App() {
+  type WeatherData = {
+    name: string;
+    Country: string;
+    temp: number;
+    temp_min: number;
+    temp_max: number;
+    humidity: number;
+    pressure: number;
+    visibility: number;
+    speed: number;
+    deg: number;
+    sunrise: number;
+    sunset: number;
+    description: string;
+    main: string;
+    icon: string;
+    time: number;
+  };
+  const [city, setCity] = useState("");
+  const [country, setCountry] = useState("");
+  const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [error, setError] = useState(false);
+
+  useEffect(() => {
+    const cty = "bengaluru";
+    const cntry = "india";
+    (async () => {
+      await axios
+        .get(
+          `https://api.openweathermap.org/data/2.5/weather?q=${cty},${cntry}&appid=${process.env.REACT_APP_API_KEY}`
+        )
+        .then((res) => {
+          console.log(res.data);
+
+          const data = res.data;
+          const name = data.name;
+          const Country = data.sys.country;
+          const sunrise = data.sys.sunrise;
+          const sunset = data.sys.sunset;
+          const temp = data.main.temp;
+          const temp_min = data.main.temp_min;
+          const temp_max = data.main.temp_max;
+          const humidity = data.main.humidity;
+          const pressure = data.main.pressure;
+          const visibility = data.visibility;
+          const speed = data.wind.speed;
+          const deg = data.wind.deg;
+          const description = data.weather[0].description;
+          const main = data.weather[0].main;
+          const icon = data.weather[0].icon;
+          const time = data.dt;
+
+          const weatherData: WeatherData = {
+            name,
+            Country,
+            temp,
+            temp_min,
+            temp_max,
+            humidity,
+            pressure,
+            visibility,
+            sunrise,
+            sunset,
+            speed,
+            deg,
+            description,
+            main,
+            icon,
+            time,
+          };
+
+          setWeatherData(weatherData);
+        })
+        .catch((err) => {
+          console.log("hello err");
+        });
+    })();
+  }, []);
+
+  const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    try {
+      const res = await axios.get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${process.env.REACT_APP_API_KEY}`
+      );
+
+      const data = res.data;
+      const name = data.name;
+      const Country = data.sys.country;
+      const sunrise = data.sys.sunrise;
+      const sunset = data.sys.sunset;
+      const temp = data.main.temp;
+      const temp_min = data.main.temp_min;
+      const temp_max = data.main.temp_max;
+      const humidity = data.main.humidity;
+      const pressure = data.main.pressure;
+      const visibility = data.visibility;
+      const speed = data.wind.speed;
+      const deg = data.wind.deg;
+      const description = data.weather[0].description;
+      const main = data.weather[0].main;
+      const icon = data.weather[0].icon;
+      const time = data.dt;
+
+      const weatherData: WeatherData = {
+        name,
+        Country,
+        temp,
+        temp_min,
+        temp_max,
+        humidity,
+        pressure,
+        visibility,
+        sunrise,
+        sunset,
+        speed,
+        deg,
+        description,
+        main,
+        icon,
+        time,
+      };
+
+      setWeatherData(weatherData);
+    } catch (err) {
+      console.log("hello err");
+      setError(true);
+    }
+    setCity("");
+    setCountry("");
+  };
+
+  console.log(weatherData);
+
+  return (
+    <Wrapper>
+      <Header>Weather App</Header>
+
+      <From onSubmit={handleSubmit}>
+        <Input
+          type="text"
+          placeholder="City"
+          value={city}
+          onChange={(e) => setCity(e.target.value)}
+        />
+        <Input
+          type="text"
+          placeholder="Country"
+          value={country}
+          onChange={(e) => setCountry(e.target.value)}
+        />
+        <Button type="submit">Submit</Button>
+      </From>
+
+      <Report>
+        <div>
+          {weatherData?.name} , {weatherData?.Country}. Weather
+        </div>
+        <Time>
+          As of{" "}
+          {weatherData && new Date(weatherData.time * 1000).getHours() < 12
+            ? weatherData &&
+              `${Math.abs(
+                new Date(weatherData.time * 1000).getHours()
+              )}:${new Date(weatherData.time * 1000).getMinutes()}:${new Date(
+                weatherData.time * 1000
+              ).getSeconds()} AM`
+            : weatherData &&
+              `${Math.abs(
+                24 - 12 - new Date(weatherData.time * 1000).getHours()
+              )}:${new Date(weatherData.time * 1000).getMinutes()}:${new Date(
+                weatherData.time * 1000
+              ).getSeconds()} PM`}
+        </Time>
+
+        <Temp>
+          <Degree>
+            {weatherData && `${(weatherData?.temp - 273.15).toFixed(0)}°`}
+          </Degree>
+          <Icon>
+            <img
+              style={{ width: 38 }}
+              src={`http://openweathermap.org/img/wn/${weatherData?.icon}@2x.png`}
+              alt={`${weatherData?.main}`}
+            />
+            <span style={{ fontSize: 14 }}>{weatherData?.main}</span>
+          </Icon>
+        </Temp>
+        <span style={{ fontSize: 17 }}>{weatherData?.main.toLowerCase()}</span>
+      </Report>
+
+      {weatherData && (
+        <Data>
+          <Grid>
+            <b>High/Low </b>
+            <span>
+              {(weatherData.temp_max - 273.15).toFixed(0)}/
+              {(weatherData.temp_min - 273.15).toFixed(0)}
+            </span>
+          </Grid>
+          <Grid>
+            <b>Wind</b>
+            <span>{weatherData.speed} Km/hr</span>
+          </Grid>
+          <Grid>
+            <b>Humidity</b>
+            <span>{weatherData.humidity} %</span>
+          </Grid>
+          <Grid>
+            <b>Wind Direction</b>
+            <span>{weatherData.deg} ° deg</span>
+          </Grid>
+          <Grid>
+            <b>Pressure</b>
+            <span>{weatherData.pressure} hpa</span>
+          </Grid>
+          <Grid>
+            <b>Sunrise</b>
+            <span>
+              {`${new Date(weatherData.sunrise * 1000).getHours()}:${new Date(
+                weatherData.sunrise * 1000
+              ).getMinutes()}:${new Date(
+                weatherData.sunrise * 1000
+              ).getSeconds()}`}{" "}
+              AM
+            </span>
+          </Grid>
+          <Grid>
+            <b>Visibility</b>
+            <span>{weatherData.visibility / 1000} Km</span>
+          </Grid>
+          <Grid>
+            <b>Sunset</b>
+            <span>
+              {`${Math.abs(
+                24 - 12 - new Date(weatherData.sunset * 1000).getHours()
+              )}:${new Date(weatherData.sunset * 1000).getMinutes()}:${new Date(
+                weatherData.sunset * 1000
+              ).getSeconds()}`}{" "}
+              PM
+            </span>
+          </Grid>
+        </Data>
+      )}
+    </Wrapper>
+  );
+}
+
+export default App;
